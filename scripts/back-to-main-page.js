16f3abf10973eb887efe0a685114805b0456135d
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2023-10-17 14:39:37 +0300

    Возврат на главную JS

---
+++ scripts/back-to-main-page.js
@@ -0,0 +1,5 @@
+let backToMainPage = document.querySelector("#back-to-general-page");
+
+backToMainPage.addEventListener("click", function () {
+  location.href = "../index.html";
+});
--- scripts/task6.js
+++
@@ -1,5 +0,0 @@
-let task6 = document.querySelector("#back-to-general-page");
-
-task6.addEventListener("click", function () {
-  location.href = "../index.html";
-});
