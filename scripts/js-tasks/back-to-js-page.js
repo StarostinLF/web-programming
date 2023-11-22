b56761a15cd873befc14853f436129c580b1c2dc
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2023-11-22 22:45:56 +0300

    Фикс кноки назад на странице с JS

---
+++ scripts/js-tasks/back-to-js-page.js
@@ -0,0 +1,5 @@
+const backToJSPage = document.querySelector("#back-to-JS-page");
+
+backToJSPage.addEventListener("click", () => {
+  location.href = "./javascript.html";
+});
---
+++ scripts/js-tasks/from-JS-to-main-page.js
@@ -0,0 +1,5 @@
+const fromJSToMainPage = document.querySelector("#from-JS-to-main-page");
+
+fromJSToMainPage.addEventListener("click", () => {
+  location.href = "../../index.html";
+});
